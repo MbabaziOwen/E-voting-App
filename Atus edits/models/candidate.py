84ae09e4ea8efb8a63286801953a305933c4d7c6
Aturinda Beinembabazi <beineaturinda@gmail.com>
2026-03-12 21:50:06 +0300

    Update candidate.py

--- Atus edits/models/candidate.py
+++ Atus edits/models/candidate.py
@@ -10,4 +10,26 @@ class Candidate:
         self.full_name = full_name
         self.national_id = national_id
         self.date_of_birth = date_of_birth
+        self.age =  age
+        self.gender = gender
+        self.education = education
+        self.party = party
+        self.manifesto =  manifesto
+        self.address = address
+        self.phone = phone
+        self.email = email
+        self.years_experience = years_experience
+        self.has_criminal_record = False
+        self.is_active = True
+        self.is_approved = True
+        self.created_at = created_at
+        self.created_by = created_by
         
+    def to_dict(self):
+        return self.__dict__
+    
+    @classmethod
+    def from_dict(cls, d):
+        obj = cls.__new__(cls)
+        obj.__dict__.update(d)
+        return obj
