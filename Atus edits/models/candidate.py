0eb36fc6a8bdc960e4d90d28a14e4918f3371420
Aturinda Beinembabazi <beineaturinda@gmail.com>
2026-03-12 21:40:09 +0300

    Update candidate.py

--- Atus edits/models/candidate.py
+++ Atus edits/models/candidate.py
@@ -2,5 +2,12 @@ class Candidate:
     MIN_AGE = 25
     MAX_AGE = 75
     EDUCATION_LEVELS = [
-        "Bachelor"
+        "Bachelor's Degree", "Master's Degree", " PhD", "Directorate"
     ]
+    
+    def __init__(self, id, full_name, national_id, date_of_birth, age, gender, education, party, manifesto, address, phone, email, years_experience, created_by="", created_at=""):
+        self.id = id
+        self.full_name = full_name
+        self.national_id = national_id
+        self.date_of_birth = date_of_birth
+        
